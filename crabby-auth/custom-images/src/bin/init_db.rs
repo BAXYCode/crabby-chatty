2d28a083ead2e24181ebf1d288994c098115d70d
BAXYCode <lainebenjamin3@gmail.com>
2024-05-19 22:44:51 -0400

    temp
    doesn't work properly, dns needs to be resolved dynamically through
    traefik not through docker.

--- crabby-auth/custom-images/src/bin/init_db.rs
+++ crabby-auth/custom-images/src/bin/init_db.rs
@@ -1,11 +1,11 @@
 #![allow(unused_variables)]
 use std::env;
 
-use tracing::info;
+use tracing::{info, instrument};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::registry()
+    let _sub: () = tracing_subscriber::registry()
         .with(
             tracing_subscriber::filter::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
                 // axum logs rejections from built-in extractors with the `axum::rejection`
@@ -15,10 +15,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
         .with(tracing_subscriber::fmt::layer().pretty())
         .init();
-    info!(name:"Environment Variables", "fetching variables");
+    info!(name:"Environment Variables", "fetching  necessary environment variables");
     let host = env::var("COCKROACH_HOST").expect("No Host provided");
     let port = env::var("COCKROACH_PORT").expect("No port provided");
     let _user = env::var("COCKROACH_USER").expect("OS user not provided");
+
+    let cluster_host = env::var("CLUSTER_DNS").expect("missing cluster dns");
+    let cluster_port = env::var("CLUSTER_PORT").expect("missing cluster http port");
 
     info!(name:"Environment Variables", "Connecting to cluster from {} at port number {}", host, port);
     let _insecure = env::var("COCKROACH_INSECURE");
@@ -38,6 +41,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     if let Ok(db_name) = db_name.clone() {
+        info!(name: "db initialization", "Database initialization");
+        wait_ready(cluster_host, cluster_port).expect("failed to get ready");
         let db = std::process::Command::new("/cockroach")
             .args([
                 "sql",
@@ -46,7 +51,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             ])
             .status()
             .expect("could not create Database");
-        info!(name: "db initialization", "Database initialization status: {:?}", db);
     }
 
     if let Ok(username) = db_user {
@@ -82,5 +86,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         info!(name:"permissions and roles", "Granting permissions and roles status: {:?}",privileges)
     }
 
+    Ok(())
+}
+#[instrument]
+pub fn wait_ready(host: String, port: String) -> Result<(), reqwest::Error> {
+    info!(name:"Node Boot up", "waiting on Node activation");
+    let url = format!("http://admin.roachdb.localhost:{port}/api/v2/health/");
+    loop {
+        let ready = reqwest::blocking::get(&url);
+        if let Err(e) = ready {
+            // println!("fail");
+            continue;
+        }
+        info!(name:"test", "{ready:?}");
+
+        if ready.unwrap().text()? == "{}" {
+            info!(name:"Node Boot up", "Node boot up is complete");
+            break;
+        }
+    }
     Ok(())
 }
